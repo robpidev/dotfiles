a2ade6f9732a73504a311b9a230d1b75ad496203
Rober Torres <robertdrakenfor@gmail.com>
2022-04-16 17:20:15 +0000

    poshthemes

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,4 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-oh-my-posh init fish --config ~/.poshthemes/clean-detailed.omp.json | source
+oh-my-posh init fish --config ~/.config/poshthemes/clean-detailed.omp.json | source
